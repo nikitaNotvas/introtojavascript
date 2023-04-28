bb635eabda4359e927ddbcefb3300cbcf40b8149
nikita <nvasiuta@brooklynsteamcenter.org>
2023-04-28 14:58:12 -0400

    end and start of buttons

---
+++ stopwatch.js
@@ -0,0 +1,10 @@
+function start() {
+alert("Why would you clicled me im start buttton")
+
+}
+function stop() {
+    alert("Why would you clicled me im stop button")
+    }
+function reset() { 
+   alert("Why would you clicled me im reset button!")
+    }
