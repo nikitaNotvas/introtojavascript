cd488be36bfda056f7b8c4cce998e0dcde3c44c5
nikita <nvasiuta@brooklynsteamcenter.org>
2023-05-01 14:54:13 -0400

    finished stopwatch

--- stopwatch.js
+++ stopwatch.js
@@ -1,5 +1,5 @@
 function start() {
-alert("Why would you clicled me im start buttton")
+("Why would you clicled me im start buttton")
 
 }
 function stop() {
@@ -8,3 +8,49 @@ function stop() {
 function reset() { 
    alert("Why would you clicled me im reset button!")
     }
+
+
+let mil = 0
+let sec = 0
+let min = 0
+
+let digits = document.getElementById("digits")
+
+let interval;
+
+function start () {
+stop()
+    
+interval = setInterval(function (){
+    mil = mil+10
+
+    if (mil >= 1000) {
+        sec = sec + 1
+        mil = 0
+     }
+     
+    if (sec >= 60){
+        min = min + 1
+        sec = 0
+    }
+    console.log(mil)
+    console.log(sec)
+    console.log(min)
+    digits.innerHTML = String(min).padStart(2, "0") + ":" + String(sec).padStart(2, "0") + ":" + mil/10
+
+
+
+}, 10);
+
+
+}
+function stop () {
+    clearInterval(interval)
+}
+function reset () {
+stop()
+digits.innerHTML = "00:00:00"
+min = 0
+sec = 0
+mil = 0
+}
